4894c1c04cea9a713fd9aeca9f03b243cc67061f
arman <arman503@163.com>
2019-12-15 15:54:11 +0800

    发红包案例

---
+++ 抽象、接口/src/abstrapractise/Manager.java
@@ -0,0 +1,41 @@
+package abstrapractise;
+
+import java.util.ArrayList;
+// 群主的类
+public class Manager extends User{
+    public Manager(){
+
+    }
+
+    public Manager(String name, int money) {
+        super(name, money);
+    }
+
+    public ArrayList<Integer> send(int totalMoney, int count){
+        // 首先需要一个集合，用来存储若干个红包的金额
+        ArrayList<Integer> redList = new ArrayList<>();
+
+        // 首先看一下群主自己有多少钱
+        int leftMoney = super.getMoney();  // 群主当前的余额
+        if (totalMoney > leftMoney){
+            System.out.println("余额不足");
+            return redList;
+        }
+        // 扣钱，其实就是重新设置余额
+        super.setMoney(leftMoney - totalMoney);
+
+        //发红包需要平均拆分为 count份
+        int avg = totalMoney / count;
+        int mod = totalMoney % count;
+
+        //  出不开的的零头，包在最后一个红包当中
+        //  下面把红包一个一个放到集合当中
+        for (int i = 0; i < count; i ++){
+            redList.add(avg);
+        }
+        // 最后一个红包
+        int last = avg + mod;
+        redList.add(last);
+        return redList;
+    }
+}
---
+++ 抽象、接口/src/abstrapractise/Member.java
@@ -0,0 +1,31 @@
+package abstrapractise;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Member extends User {
+    public Member() {
+    }
+
+    public Member(String name, int money) {
+        super(name, money);
+    }
+
+    public void receive(ArrayList<Integer> list){
+        //从多个红包当中 随机抽取一个，给我自己
+        //随机获取一个集合当中的索引编号
+        int index = new Random().nextInt(list.size());
+
+        //根据索引，从集合当中删除，并且得到已被删除的红包，给我自己
+        int delta =  list.remove(index);
+
+        // 当前成员字节本来有多少钱， 在加给自己
+        int money = super.getMoney();
+
+        // 加法，并且重新设置回去
+        super.setMoney(money + delta);
+
+
+
+    }
+}

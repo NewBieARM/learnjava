3dcbd9580f6adc35c05033eedc76e22d7b45f9c7
arman <arman503@163.com>
2019-12-15 09:54:31 +0800

    继承中 各个变量的访问方法

---
+++ 进阶玩法/src/extendsdemo/ExtendsField.java
@@ -0,0 +1,12 @@
+package extendsdemo;
+/*
+局部变量：           直接写成员变量
+本类的成员类变量：     this.成员变量名
+父类的成员变量：       super.成员变量名
+ */
+public class ExtendsField {
+    public static void main(String[] args) {
+        Zi z = new Zi();
+        z.method();
+    }
+}
---
+++ 进阶玩法/src/extendsdemo/Fu.java
@@ -0,0 +1,6 @@
+package extendsdemo;
+
+public class Fu {
+    int num = 10;
+
+}

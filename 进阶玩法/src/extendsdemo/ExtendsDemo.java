e07822660b2358eec9637f56fda2db3e5e4a37eb
arman <arman503@163.com>
2019-12-15 09:34:00 +0800

    继承的概述

---
+++ 进阶玩法/src/extendsdemo/ExtendsDemo.java
@@ -0,0 +1,16 @@
+package extendsdemo;
+/*
+在继承的关系中，子类就是父类：子类可一被当作父类看待
+
+父类格式：
+public class 父类名称{}
+
+子类格式：
+public class 子类名称 extends 父类名称{}
+ */
+public class ExtendsDemo {
+    public static void main(String[] args) {
+        Teacher th = new Teacher();
+        th.method();
+    }
+}
---
+++ 进阶玩法/src/extendsdemo/Teacher.java
@@ -0,0 +1,7 @@
+package extendsdemo;
+/*
+定义了一个员工类
+ */
+public class Teacher extends Employee{
+
+}

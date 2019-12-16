be9f16ef6212ef577943990d3c98bc630d085f19
arman <arman503@163.com>
2019-12-16 10:49:25 +0800

    接口概括、注意事项

---
+++ 抽象、接口/src/interfacedemo/InterfaceRun.java
@@ -0,0 +1,10 @@
+package interfacedemo;
+
+public class InterfaceRun {
+    public static void main(String[] args) {
+
+        MyInterfaceAbsImpl impl = new MyInterfaceAbsImpl();
+        impl.methodAbs();
+
+    }
+}

6859df47424a9c182d9a733ab07e9b9af465d1b4
arman <arman503@163.com>
2019-12-24 15:37:20 +0800

    含有泛型的接口

--- 集合框架/src/泛型/RunGenericInterfaceImpl2.java
+++ 集合框架/src/泛型/RunGenericInterfaceImpl2.java
@@ -4,7 +4,7 @@ package 泛型;
 public class RunGenericInterfaceImpl2 {
     public static void main(String[] args) {
         GenericInterfaceImpl2<String> impl2 = new GenericInterfaceImpl2<String>();
-        impl2.method("sadsad");
+        impl2.method("alex");
 
         GenericInterfaceImpl2<Double> impl21 = new GenericInterfaceImpl2<>();
         impl21.method(2.333);

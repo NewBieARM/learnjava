c5e70dc1fd8ab70eb0216272bb911a1e33851ab7
arman <arman503@163.com>
2019-12-24 15:35:34 +0800

    含有泛型的方法

--- 集合框架/src/泛型/RunGenericMethod.java
+++ 集合框架/src/泛型/RunGenericMethod.java
@@ -9,9 +9,6 @@ public class RunGenericMethod {
         gm.method01('a');
         gm.method01(true);
 
-
-        gm.method01("静态方法， 不建议创建对象使用");
-
         // 静态方法，通过类名.方法名(参数)  可以直接使用
         GenericMethod.method02("这是静态方法");
 

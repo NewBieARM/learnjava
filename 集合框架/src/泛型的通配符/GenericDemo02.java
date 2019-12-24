33894b860ca157af61159e1d31b537f6a91646e4
arman <arman503@163.com>
2019-12-24 16:15:17 +0800

    泛型上限限定和下限限定

--- 集合框架/src/泛型的通配符/GenericDemo02.java
+++ 集合框架/src/泛型的通配符/GenericDemo02.java
@@ -15,18 +15,18 @@ public class GenericDemo02 {
         Collection<Object> list4 = new ArrayList<Object>();
 
         getElement1(list1);
-        getElement1(list2);//报错
+//        getElement1(list2);//报错
         getElement1(list3);
-        getElement1(list4);//报错
+//        getElement1(list4);//报错
 
-        getElement2(list1);//报错
-        getElement2(list2);//报错
+//        getElement2(list1);//报错
+//        getElement2(list2);//报错
         getElement2(list3);
         getElement2(list4);
 
     }
 
-    }
+
 
     // 泛型的上限：此时的泛型?，必须是Number类型或者Number类型的子类
     public static void getElement1(Collection<? extends Number> coll){}

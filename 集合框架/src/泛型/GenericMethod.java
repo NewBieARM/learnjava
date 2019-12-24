0f91a0878ce78f955571eea468d73882074aab3a
arman <arman503@163.com>
2019-12-24 14:45:41 +0800

    含有泛型的普通方法和静态方法

--- 集合框架/src/泛型/GenericMethod.java
+++ 集合框架/src/泛型/GenericMethod.java
@@ -12,4 +12,9 @@ public class GenericMethod {
         System.out.println(m);
     }
 
+    //定义一个含有泛型的的静态方法
+    public static <S> void method02(S s){
+        System.out.println(s);
+    }
+
 }
--- 集合框架/src/泛型/RunGenericMethod.java
+++ 集合框架/src/泛型/RunGenericMethod.java
@@ -2,12 +2,18 @@ package 泛型;
 
 public class RunGenericMethod {
     public static void main(String[] args) {
-        GenericMethod gc = new GenericMethod();
+        GenericMethod gm = new GenericMethod();
 
-        gc.method01("alex");
-        gc.method01(123);
-        gc.method01('a');
-        gc.method01(true);
+        gm.method01("alex");
+        gm.method01(123);
+        gm.method01('a');
+        gm.method01(true);
+
+
+        gm.method01("静态方法， 不建议创建对象使用");
+
+        // 静态方法，通过类名.方法名(参数)  可以直接使用
+        GenericMethod.method02("这是静态方法");
 
     }
 }

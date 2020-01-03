fcebd4e8fc3b0725f15848a44ceca9e846248aaa
arman <arman503@163.com>
2020-01-03 11:46:42 +0800

    线程安全问题 Lock接口

---
+++ 并发与并行/src/LockDemo/TicketDemo.java
@@ -0,0 +1,13 @@
+package LockDemo;
+
+public class TicketDemo {
+    public static void main(String[] args) {
+        RunnableImpl run = new RunnableImpl();
+        Thread t0 = new Thread(run, "T0");
+        Thread t1 = new Thread(run, "T1");
+        Thread t2 = new Thread(run, "T2");
+        t0.start();
+        t1.start();
+        t2.start();
+    }
+}

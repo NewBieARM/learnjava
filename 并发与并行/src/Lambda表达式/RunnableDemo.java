bc2984db1943cf4cd644f7d5538f9de47fff6c8f
arman <arman503@163.com>
2020-01-18 13:59:14 +0800

    面向对象创建多线程

---
+++ 并发与并行/src/Lambda表达式/RunnableDemo.java
@@ -0,0 +1,24 @@
+package Lambda表达式;
+
+public class RunnableDemo {
+    public static void main(String[] args) {
+        // 创建 Runnable 接口的实现类对象
+        RunnableImpl impl = new RunnableImpl();
+        // 创建Thread类对象,构造方法中传递 Runnable 接口的实现类
+        Thread t = new Thread(impl);
+        // 开启新线程,执行run方法
+        t.start();
+
+
+
+        // 简化代码,使用匿名内部类实现多线程程序
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "新线程任务创建");
+
+            }
+        };
+        new Thread(r).start();
+    }
+}
---
+++ 并发与并行/src/Lambda表达式/RunnableImpl.java
@@ -0,0 +1,8 @@
+package Lambda表达式;
+
+public class RunnableImpl implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "新线程任务创建");
+    }
+}

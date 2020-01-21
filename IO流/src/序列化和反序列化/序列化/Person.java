92eddc07af8c47402c762ce44131fbdaf7569527
arman <arman503@163.com>
2020-01-21 17:34:57 +0800

    对象的反序列化

--- IO流/src/序列化和反序列化/序列化/Person.java
+++ IO流/src/序列化和反序列化/序列化/Person.java
@@ -11,6 +11,13 @@ import java.io.Serializable;
             有:可以序列化或反序列化
             没有: 就会抛出NotSerializableException异常
 
+    static 关键字:静态关键字
+        静态关键字优先于非静态加载到内存中(静态优先于对象进入到内存中)
+        被static修饰的成员变量不能被序列化的,序列化的都是对象
+
+    transient关键字:瞬态关键字
+        被transient修饰的成员变量,不能被序列化
+
  */
 public class Person implements Serializable {
     private String name;
